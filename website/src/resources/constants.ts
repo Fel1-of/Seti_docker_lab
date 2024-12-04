d405876be2de4b055a3fdd7bdffe3b39f1e8982c
spanwalla <lus550@yandex.ru>
2024-12-04 13:02:49 +0300

    добавил том и healthcheck

--- website/src/resources/constants.ts
+++ website/src/resources/constants.ts
@@ -1,5 +1,5 @@
 const SDOW_API_URL_DEV = 'http://127.0.0.1:5000';
-const SDOW_API_URL_PROD = process.env.API_URL || 'https://api.sixdegreesofwikipedia.com';
+const SDOW_API_URL_PROD = process.env.API_URL;
 
 export const SDOW_API_URL =
   process.env.NODE_ENV === 'production' ? SDOW_API_URL_PROD : SDOW_API_URL_DEV;
