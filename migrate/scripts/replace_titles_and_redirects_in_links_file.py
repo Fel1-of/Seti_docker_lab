b4ae9daa639f285418ffea2109eada387b900048
Fel1-of <fivehellday@gmail.com>
2024-12-05 01:35:00 +0300

    Fix, but 127.0.0.1 error

--- migrate/scripts/replace_titles_and_redirects_in_links_file.py
+++ migrate/scripts/replace_titles_and_redirects_in_links_file.py
@@ -34,29 +34,35 @@ if not LINKS_FILE.endswith('.gz'):
 # Create a set of all page IDs and a dictionary of page titles to their corresponding IDs.
 ALL_PAGE_IDS = set()
 PAGE_TITLES_TO_IDS = {}
-for line in io.BufferedReader(gzip.open(PAGES_FILE, 'r')):
-  [page_id, page_title, _] = line.rstrip('\n').split('\t')
-  ALL_PAGE_IDS.add(page_id)
-  PAGE_TITLES_TO_IDS[page_title] = page_id
+with gzip.open(PAGES_FILE, 'rt', encoding='utf-8') as pages_file:
+    for line in pages_file:
+        page_id, page_title, _ = line.rstrip('\n').split('\t')
+        ALL_PAGE_IDS.add(page_id)
+        PAGE_TITLES_TO_IDS[page_title] = page_id
 
 # Create a dictionary of page IDs to the target page ID to which they redirect.
 REDIRECTS = {}
-for line in io.BufferedReader(gzip.open(REDIRECTS_FILE, 'r')):
-  [source_page_id, target_page_id] = line.rstrip('\n').split('\t')
-  REDIRECTS[source_page_id] = target_page_id
+with gzip.open(REDIRECTS_FILE, 'rt', encoding='utf-8') as redirects_file:
+    for line in redirects_file:
+        source_page_id, target_page_id = line.rstrip('\n').split('\t')
+        REDIRECTS[source_page_id] = target_page_id
 
 # Loop through each line in the links file, replacing titles with IDs, applying redirects, and
 # removing nonexistent pages, writing the result to stdout.
-for line in io.BufferedReader(gzip.open(LINKS_FILE, 'r')):
-  [source_page_id, target_page_title] = line.rstrip('\n').split('\t')
+with gzip.open(LINKS_FILE, 'rt', encoding='utf-8') as links_file:
+    for line in links_file:
+        source_page_id, target_page_title = line.rstrip('\n').split('\t')
 
-  source_page_exists = source_page_id in ALL_PAGE_IDS
+        source_page_exists = source_page_id in ALL_PAGE_IDS
 
-  if source_page_exists:
-    source_page_id = REDIRECTS.get(source_page_id, source_page_id)
+        if source_page_exists:
+            # Apply redirects for the source page
+            source_page_id = REDIRECTS.get(source_page_id, source_page_id)
 
-    target_page_id = PAGE_TITLES_TO_IDS.get(target_page_title)
+            # Find the target page ID
+            target_page_id = PAGE_TITLES_TO_IDS.get(target_page_title)
 
-    if target_page_id is not None and source_page_id != target_page_id:
-      target_page_id = REDIRECTS.get(target_page_id, target_page_id)
-      print('\t'.join([source_page_id, target_page_id]))
+            if target_page_id is not None and source_page_id != target_page_id:
+                # Apply redirects for the target page
+                target_page_id = REDIRECTS.get(target_page_id, target_page_id)
+                print('\t'.join([source_page_id, target_page_id]))
